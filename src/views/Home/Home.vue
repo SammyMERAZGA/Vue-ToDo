eb90b32ccd08ea4734ae335a1b2b1cb6ca187552
Sammy Merazga <sammy.merazga@gmail.com>
2023-03-29 13:22:37 +0200

    todo page done

--- src/views/Home/Home.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <h1 class="text-center">To Do List App</h1>
-</template>
-
-<script lang="ts" src="./index.ts"></script>
-<style scoped src="./style.css"></style>
-
--- src/views/Home/index.ts
+++ src/views/Home/index.ts
@@ -1,3 +1,32 @@
 import Vue from "vue";
+import { Todo } from "@/types/todo";
 
-export default class Home extends Vue {}
+export default class HomeView extends Vue {
+  newTodoTitle = "";
+
+  todos: Todo[] = [];
+
+  doneTodo(id: number) {
+    // eslint-disable-next-line
+    let todo = this.todos.filter((todo: Todo) => todo.id === id)[0];
+    todo.done = !todo.done;
+    console.log(`🚀 id: ${id}`);
+  }
+
+  addTodo() {
+    // eslint-disable-next-line
+    let newTodo = {
+      id: Date.now(),
+      text: this.newTodoTitle,
+      done: false,
+    };
+    this.todos.push(newTodo);
+    this.newTodoTitle = "";
+    console.log("🚀 Todo added");
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((todo: Todo) => todo.id !== id);
+    console.log("🚀 Todo deleted");
+  }
+}
